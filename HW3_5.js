2bd2f539c738b8956b885b9fc6c8dde2c011ffb3
shangria <shangrialena@gmail,com>
2020-01-20 22:55:42 +0200

    add array fill

--- HW3_5.js
+++ HW3_5.js
@@ -1,9 +1,10 @@
 // array fill
 // Создайте пустой массив и добавляйте в него элементы, пока пользователь не нажмет Отмена в очередном prompt. Используйте push для удобства: push
 var arr = [];
-var i = 0;
-while (i = prompt('введите что-нибудь')) {
-    arr.push(i)
+var inp = prompt('введите что-нибудь');
+while (inp != undefined) {
+    arr.push(inp);
+    inp = prompt('введите что-нибудь');
 }
 console.log(arr);
 
